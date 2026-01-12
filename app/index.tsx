4b55130aac48fc4118a6be9fc513e04cc6905dca
Marco Vázquez <up230432@alumnos.upa.edu.mx>
2026-01-12 11:40:29 -0600

    feat: implement theme context with async storage and add button component

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 import { Text, View } from 'react-native'
 
 const index = () => {
   return (
-    <View className='pt-10'>
-        <Text className='text-red-500'>Index Page</Text>
+    <View className='pt-10 px-4'>
+        <Text className='text-red-500 mb-4'>Index Page</Text>
+        <Button variant='secondary' className='mb-4'>
+          <Text className='text-red-300'>Default Button</Text>
+        </Button>
     </View>
   )
 }
